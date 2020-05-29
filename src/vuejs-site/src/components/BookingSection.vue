9a8b0a34169c7f16461620e369451da5d4fa3ea3
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-05-29 16:23:19 +0200

    Added booking options

--- src/vuejs-site/src/components/BookingSection.vue
+++ src/vuejs-site/src/components/BookingSection.vue
@@ -1,34 +1,58 @@
 <template>
   <section class="align-center">
     <h2>{{ text.header[language] }}</h2>
-
-    <p>This section is not yet finished.</p>
+    
+    <ul>
+      <li v-for="restaurant in restaurants" :key="restaurant.area">
+        <RestaurantData :area="restaurant.area" :phoneNumber="restaurant.phoneNumber" :address="restaurant.address"/>
+      </li>
+    </ul>
   </section>
 </template>
 
 <script>
 import { languageMixin } from "../mixins/languageMixin.js";
+import RestaurantData from "./RestaurantData.vue";
 
 export default {
   mixins: [languageMixin],
+  components: {
+    RestaurantData
+  },
   data: function() {
     return {
       text: {
         header: {
-          "en-US": "BOOK A TABLE WITH US",
-          "es-ES": "RESERVA MESA CON NOSOTROS"
+          "en-US": "BOOKING",
+          "es-ES": "RESERVAS"
         }
-      }
+      },
+      restaurants: [
+        { area: "COSTANERA", phoneNumber: "TEL. (5411) 4433-0666", address: "Rafael Obligado 1032" },
+        { area: "PUERTO MADERO", phoneNumber: "TEL. (5411) 4433-0667", address: "Av. Olga Cossettini 23" },
+      ]
     };
   }
 };
 </script>
 
 <style scoped>
+h2 {
+  color: var(--accent-color-9);
+  font-size: 3rem;
+}
 section {
   background-image: url("../assets/img/bg-booking.jpg");
   background-position: center;
   display: flex;
   height: var(--default-height);
+}
+
+ul {
+  display: grid;
+  gap: 2.5rem;
+  grid-template-columns: repeat(2, 1fr);
+  list-style: none;
+  padding: 0;
 }
 </style>
---
+++ src/vuejs-site/src/components/RestaurantData.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <article class="align-center">
+      <h2>{{ instanceArea }}</h2>
+      <p>{{ instancePhone }}</p>
+      <address>{{ instanceAddress }}</address>
+    </article>
+
+    <input type="button" class="button" :value="text.button[language]" />
+  </div>
+</template>
+
+<script>
+import { languageMixin } from "../mixins/languageMixin.js";
+
+export default {
+  mixins: [languageMixin],
+  props: ["area", "phoneNumber", "address"],
+  data: function() {
+    return {
+      instanceArea: this.area,
+      instancePhone: this.phoneNumber,
+      instanceAddress: this.address,
+      text: {
+        button: {
+          "en-US": "BOOK TABLE",
+          "es-ES": "RESERVAR"
+        }
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+address,
+h2 {
+  font-size: small;
+  font-style: normal;
+}
+
+article {
+  background-image: url(../assets/img/bg_restaurant.png);
+  background-position-y: 6px;
+  background-repeat: no-repeat;
+}
+
+input {
+  margin-top: 2rem;
+  background-color: black;
+}
+
+p {
+  color: var(--accent-color-9);
+  font-weight: bold;
+  padding: 0 2rem;
+}
+</style>
