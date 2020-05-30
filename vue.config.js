a94c3f5e99138675a838d44e6f8f86dfb145bc3a
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-05-30 13:53:28 +0200

    New build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/i-piaceri-restaurant/'
+        ? '/docs/i-piaceri-restaurant/'
         : '/',
     outputDir: "docs"
 };
